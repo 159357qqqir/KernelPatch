8c7a243f671d568de540df306339a7a8a5efbb1c
159357qqqir <18344056164@163.com>
2026-09-16 04:01:38 +0800

    Update hello.c

--- kpms/demo-hello/hello.c
+++ kpms/demo-hello/hello.c
@@ -1,7 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
-/* 
- * Copyright (C) 2023 bmax121. All Rights Reserved.
- */
 
 #include <compiler.h>
 #include <kpmodule.h>
@@ -10,58 +7,23 @@
 #include <kputils.h>
 #include <linux/string.h>
 
-///< The name of the module, each KPM must has a unique name.
-KPM_NAME("kpm-hello-demo");
-
-///< The version of the module.
+KPM_NAME("vtouch-demo");
 KPM_VERSION("1.0.0");
-
-///< The license type.
 KPM_LICENSE("GPL v2");
+KPM_AUTHOR("me");
+KPM_DESCRIPTION("Virtual Touch Test Module");
 
-///< The author.
-KPM_AUTHOR("bmax121");
-
-///< The description.
-KPM_DESCRIPTION("KernelPatch Module Example");
-
-/**
- * @brief hello world initialization
- * @details 
- * 
- * @param args 
- * @param reserved 
- * @return int 
- */
-static long hello_init(const char *args, const char *event, void *__user reserved)
+static long vtouch_init(const char *args, const char *event, void *__user reserved)
 {
-    pr_info("kpm hello init, event: %s, args: %s\n", event, args);
-    pr_info("kernelpatch version: %x\n", kpver);
+    pr_info("vtouch-demo: module loaded!\n");
     return 0;
 }
 
-static long hello_control0(const char *args, char *__user out_msg, int outlen)
+static long vtouch_exit(void *__user reserved)
 {
-    pr_info("kpm hello control0, args: %s\n", args);
-    char echo[64] = "echo: ";
-    strncat(echo, args, 48);
-    compat_copy_to_user(out_msg, echo, sizeof(echo));
+    pr_info("vtouch-demo: module unloaded!\n");
     return 0;
 }
 
-static long hello_control1(void *a1, void *a2, void *a3)
-{
-    pr_info("kpm hello control1, a1: %llx, a2: %llx, a3: %llx\n", a1, a2, a3);
-    return 0;
-}
-
-static long hello_exit(void *__user reserved)
-{
-    pr_info("kpm hello exit\n");
-    return 0;
-}
-
-KPM_INIT(hello_init);
-KPM_CTL0(hello_control0);
-KPM_CTL1(hello_control1);
-KPM_EXIT(hello_exit);
+KPM_INIT(vtouch_init);
+KPM_EXIT(vtouch_exit);
