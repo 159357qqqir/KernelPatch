b4ef3082a55d4ed9e1ce4860eeaf943362d32e61
159357qqqir <18344056164@163.com>
2026-09-16 04:14:57 +0800

    Update hello.c

--- kpms/demo-hello/hello.c
+++ kpms/demo-hello/hello.c
@@ -1,30 +1,18 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
-
-#include <compiler.h>
 #include <kpmodule.h>
-#include <linux/printk.h>
-#include <common.h>
-#include <kputils.h>
-#include <linux/string.h>
 
-KPM_NAME("vtouch-demo");
-KPM_VERSION("1.0.0");
-KPM_LICENSE("GPL v2");
-KPM_AUTHOR("me");
-KPM_DESCRIPTION("Virtual Touch Test Module");
+KPM_NAME("minimal_test");
+KPM_VERSION("1.0");
 
-static long vtouch_init(const char *args, const char *event, void *__user reserved)
+static int kpm_init(void)
 {
-    pr_info("vtouch-demo: module loaded!\n");
+    kp_info("✅ Minimal KPM loaded!\n");
     return 0;
 }
 
-static long vtouch_exit(void *__user reserved)
+static void kpm_exit(void)
 {
-    pr_info("vtouch-demo: module unloaded!\n");
-    return 0;
+    kp_info("❌ Minimal KPM unloaded!\n");
 }
 
-KPM_INIT(vtouch_init);
-KPM_EXIT(vtouch_exit);
-
+KPM_INIT(kpm_init);
+KPM_EXIT(kpm_exit);
